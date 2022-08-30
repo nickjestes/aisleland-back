8a2fbbb95626d09aa9e0ae3577a7f84cc6cb96fd
DanTheDev-zsh <XuDanDev@gmail.com>
2022-08-30 11:57:58 -0700

    created more seedings

--- utils/data.js
+++ utils/data.js
@@ -1,37 +1,384 @@
-let foods = [
+let foods_store1 = [
     {
         typeName: 'Bread',
-        aisleLocation: '4',
+        aisleLocation: '2',
     },
     {
         typeName: 'Cereal',
-        aisleLocation: '4'
+        aisleLocation: '2'
     },
     {
         typeName: 'Chips',
         aisleLocation: '6'
+    },
+    {
+        typeName: 'Granola Bars',
+        aisleLocation: '6'
+    },
+    {
+        typeName: 'Pasta',
+        aisleLocation: '5'
+    },
+    {
+        typeName: 'Jams',
+        aisleLocation: '2'
+    },
+    {
+        typeName: 'Coffee',
+        aisleLocation: '1'
+    },
+    {
+        typeName: 'Tea',
+        aisleLocation: '1'
+    },
+    {
+        typeName: 'Baking',
+        aisleLocation: '2'
+    },
+    {
+        typeName: 'Bulk Foods',
+        aisleLocation: '1'
+    },
+    {
+        typeName: 'Rice',
+        aisleLocation: '2'
+    },
+    {
+        typeName: 'Condiments',
+        aisleLocation: '6'
+    },
+    {
+        typeName: 'Salad Dressing',
+        aisleLocation: '5'
+    },
+    {
+        typeName: 'Eggs',
+        aisleLocation: 'in the back right from main entrance'
+    },
+    {
+        typeName: 'Juice',
+        aisleLocation: 'in the back left from main entrance'
+    },
+    {
+        typeName: 'Butter',
+        aisleLocation: 'in the back right from main entrance'
+    },
+    {
+        typeName: 'Canned Tomatoes',
+        aisleLocation: '6'
+    },
+    {
+        typeName: 'Canned Beans',
+        aisleLocation: '6'
+    },
+    {
+        typeName: 'Canned Peas',
+        aisleLocation: '6'
+    },
+    {
+        typeName: 'Canned Peaches',
+        aisleLocation: '6'
+    },
+    {
+        typeName: 'Canned Tuna',
+        aisleLocation: '6'
+    },
+    {
+        typeName: 'Canned Chili',
+        aisleLocation: '6'
+    },
+    {
+        typeName: 'Canned Soup',
+        aisleLocation: '6'
+    },
+    {
+        typeName: 'Frozen Pizza',
+        aisleLocation: '5'
+    },
+    {
+        typeName: 'Frozen Single Meals',
+        aisleLocation: '5'
+    },
+    {
+        typeName: 'Frozen Burritos',
+        aisleLocation: '5'
+    },
+    {
+        typeName: 'Frozen Family Meals',
+        aisleLocation: '5'
+    },
+    {
+        typeName: 'Frozen Vegetables',
+        aisleLocation: '5'
+    },
+    {
+        typeName: 'Frozen Meats',
+        aisleLocation: '5'
     }
 ];
 
-let householdItems = [
+let householdItems_store1 = [
     {
         typeName: 'Baby Formula',
-        aisleLocation: '2'
+        aisleLocation: '4'
+    },
+    {
+        typeName: 'Baby Diapers',
+        aisleLocation: '4'
     },
     {
         typeName: 'Hair Products',
+        aisleLocation: '1'
+    },
+    {
+        typeName: 'Cold and Flu',
+        aisleLocation: '1'
+    },
+    {
+        typeName: 'Pain Medication',
+        aisleLocation: '1'
+    },
+    {
+        typeName: 'Contraception',
+        aisleLocation: '1'
+    },
+    {
+        typeName: 'Brooms and Mops',
         aisleLocation: '2'
     },
     {
-        typeName: 'Cold and Flu/Medicines',
+        typeName: 'Toilet Paper',
+        aisleLocation: '2'
+    },
+    {
+        typeName: 'Garbage Bags',
+        aisleLocation: '2'
+    },
+    {
+        typeName: 'Cleaners',
+        aisleLocation: '2'
+    },
+    {
+        typeName: 'Fabric Softener',
+        aisleLocation: '2'
+    },
+    {
+        typeName: 'Detergent',
+        aisleLocation: '2'
+    },
+    {
+        typeName: 'Sponges',
+        aisleLocation: '2'
+    },
+    {
+        typeName: 'Rubber Gloves',
+        aisleLocation: '2'
+    },
+    {
+        typeName: 'Napkins',
+        aisleLocation: '2'
+    },
+    {
+        typeName: 'Paper Towels',
+        aisleLocation: '2'
+    }    
+]
+
+let foods_store2 = [
+    {
+        typeName: 'Bread',
+        aisleLocation: '6',
+    },
+    {
+        typeName: 'Cereal',
+        aisleLocation: '6'
+    },
+    {
+        typeName: 'Chips',
+        aisleLocation: '6'
+    },
+    {
+        typeName: 'Granola Bars',
+        aisleLocation: '6'
+    },
+    {
+        typeName: 'Pasta',
+        aisleLocation: '5'
+    },
+    {
+        typeName: 'Jams',
+        aisleLocation: '2'
+    },
+    {
+        typeName: 'Coffee',
         aisleLocation: '1'
+    },
+    {
+        typeName: 'Tea',
+        aisleLocation: '1'
+    },
+    {
+        typeName: 'Baking',
+        aisleLocation: '2'
+    },
+    {
+        typeName: 'Bulk Foods',
+        aisleLocation: '1'
+    },
+    {
+        typeName: 'Rice',
+        aisleLocation: '2'
+    },
+    {
+        typeName: 'Condiments',
+        aisleLocation: '6'
+    },
+    {
+        typeName: 'Salad Dressing',
+        aisleLocation: '5'
+    },
+    {
+        typeName: 'Eggs',
+        aisleLocation: 'in the back right from main entrance'
+    },
+    {
+        typeName: 'Juice',
+        aisleLocation: 'in the back left from main entrance'
+    },
+    {
+        typeName: 'Butter',
+        aisleLocation: 'in the back right from main entrance'
+    },
+    {
+        typeName: 'Canned Tomatoes',
+        aisleLocation: '6'
+    },
+    {
+        typeName: 'Canned Beans',
+        aisleLocation: '6'
+    },
+    {
+        typeName: 'Canned Peas',
+        aisleLocation: '6'
+    },
+    {
+        typeName: 'Canned Peaches',
+        aisleLocation: '6'
+    },
+    {
+        typeName: 'Canned Tuna',
+        aisleLocation: '6'
+    },
+    {
+        typeName: 'Canned Chili',
+        aisleLocation: '6'
+    },
+    {
+        typeName: 'Canned Soup',
+        aisleLocation: '6'
+    },
+    {
+        typeName: 'Frozen Pizza',
+        aisleLocation: '5'
+    },
+    {
+        typeName: 'Frozen Single Meals',
+        aisleLocation: '5'
+    },
+    {
+        typeName: 'Frozen Burritos',
+        aisleLocation: '5'
+    },
+    {
+        typeName: 'Frozen Family Meals',
+        aisleLocation: '5'
+    },
+    {
+        typeName: 'Frozen Vegetables',
+        aisleLocation: '5'
+    },
+    {
+        typeName: 'Frozen Meats',
+        aisleLocation: '5'
     }
+];
+
+let householdItems_store2 = [
+    {
+        typeName: 'Baby Formula',
+        aisleLocation: '4'
+    },
+    {
+        typeName: 'Baby Diapers',
+        aisleLocation: '4'
+    },
+    {
+        typeName: 'Hair Products',
+        aisleLocation: '1'
+    },
+    {
+        typeName: 'Cold and Flu',
+        aisleLocation: '1'
+    },
+    {
+        typeName: 'Pain Medication',
+        aisleLocation: '1'
+    },
+    {
+        typeName: 'Contraception',
+        aisleLocation: '1'
+    },
+    {
+        typeName: 'Brooms and Mops',
+        aisleLocation: '2'
+    },
+    {
+        typeName: 'Toilet Paper',
+        aisleLocation: '2'
+    },
+    {
+        typeName: 'Garbage Bags',
+        aisleLocation: '2'
+    },
+    {
+        typeName: 'Cleaners',
+        aisleLocation: '2'
+    },
+    {
+        typeName: 'Fabric Softener',
+        aisleLocation: '2'
+    },
+    {
+        typeName: 'Detergent',
+        aisleLocation: '2'
+    },
+    {
+        typeName: 'Sponges',
+        aisleLocation: '2'
+    },
+    {
+        typeName: 'Rubber Gloves',
+        aisleLocation: '2'
+    },
+    {
+        typeName: 'Napkins',
+        aisleLocation: '2'
+    },
+    {
+        typeName: 'Paper Towels',
+        aisleLocation: '2'
+    }    
 ]
 
 
-const ALLITEMS = {
-    foodCategories: foods,
-    householdCategories: householdItems,
-};
-
+const ALLITEMS = [
+    {
+        foodCategories1: foods_store1,
+        householdCategories1: householdItems_store1,
+    },
+    {
+        foodCategories2: foods_store2,
+        householdCategories2: householdItems_store2,
+    }
+]
 module.exports = { ALLITEMS }; 
