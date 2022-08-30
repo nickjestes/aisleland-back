d443ab134978135b34ae424d2d709b396c94eb25
DanTheDev-zsh <XuDanDev@gmail.com>
2022-08-29 22:54:39 -0700

    fixed flaw with seeding and modeling

--- utils/seed.js
+++ utils/seed.js
@@ -13,26 +13,25 @@ connection.once('open', async () => {
     await Household.deleteMany({});
 
     const { foodCategories, householdCategories } = ALLITEMS;
-    // const itemsID = [];
 
-    // foodCategories.forEach(i => {
-        
-    // });
+    // create returns an array of objects created
+    const foods = await Food.create(foodCategories);
+    const households = await Household.create(householdCategories);
+    console.log({ food_ids: foods.map(x => x._id) });
+    console.log({ household_ids: households.map(x => x._id) });
 
-    const r1 = await Food.create(foodCategories);
-    console.log({r1, food_ids: r1.map(x => x._id)});
 
-    const res = await Food.collection.distinct('_id');
-    console.log(res);
+
     // const itemsID = async () => (await Food.collection.distinct('_id'));
         
-    await Household.create(householdCategories);
-
     // Add a store to the collection and await the results
     await Store.collection.insertOne({
         name: 'Safeway',
         address: '300 Bellevue Way NE, Bellevue, WA 98004',
-        allItems: ALLITEMS, 
+        allItems: {
+            foodCategories: foods.map(x => x._id),
+            householdCategories: households.map(x => x._id)
+        }, 
     });
 
 
