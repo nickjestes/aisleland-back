0a44e4c0d632d3f3213f066e01c4530f08ff47f3
Zane Sharif <Zane.Sharif@gmail.com>
2022-09-01 11:57:35 -0700

    cors added

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const db = require('./config/connection');
 const allRoutes = require('./routes'); // TODO ROUTES
+const cors = require("cors")
 
 const cwd = process.cwd();
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+app.use(cors())
 
 // Note: not necessary for the Express server to function. This just helps indicate what activity's server is running in the terminal.
 const activity = cwd.includes('aisleland-back')
